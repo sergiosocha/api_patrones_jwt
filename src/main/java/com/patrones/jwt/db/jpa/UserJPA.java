7f190367f6012a66942fbe7d8b1b1e1e53a3d6b8
sergi <sergio_socha@yahoo.com>
2025-03-05 20:47:51 -0500

    Agrega UserJPA & UserService

--- src/main/java/com/patrones/jwt/db/jpa/UserJPA.java
+++ src/main/java/com/patrones/jwt/db/jpa/UserJPA.java
@@ -1,4 +1,11 @@
 package com.patrones.jwt.db.jpa;
 
-public class UserJPA {
+import com.patrones.jwt.db.orm.UserORM;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserJPA extends JpaRepository<UserORM, Long> {
+
+
 }
--- src/main/java/com/patrones/jwt/logica/UserService.java
+++ src/main/java/com/patrones/jwt/logica/UserService.java
@@ -1,7 +1,15 @@
 package com.patrones.jwt.logica;
 
+import com.patrones.jwt.db.jpa.UserJPA;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
+@Slf4j
 public class UserService {
 
+    private final UserJPA userJPA;
 
 }
